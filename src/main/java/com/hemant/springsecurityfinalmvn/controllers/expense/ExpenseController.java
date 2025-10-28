9df35b6151848ae0f83bb322f77cf4c2027f6ac2
Hemant Medhsia <hemantmedhsia@gmail.com>
2025-10-28 17:11:49 +0530

    intregations

--- src/main/java/com/hemant/springsecurityfinalmvn/controllers/expense/ExpenseController.java
+++ src/main/java/com/hemant/springsecurityfinalmvn/controllers/expense/ExpenseController.java
@@ -1,12 +1,7 @@
 package com.hemant.springsecurityfinalmvn.controllers.expense;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.hemant.springsecurityfinalmvn.dtos.expense.AddExpenseDto;
 import com.hemant.springsecurityfinalmvn.dtos.expense.ExpenseResponseDto;
@@ -15,23 +10,49 @@ import com.hemant.springsecurityfinalmvn.services.expense.ExpenseService;
 
 import lombok.RequiredArgsConstructor;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/expense")
 @RequiredArgsConstructor
 public class ExpenseController {
-	
-	private final ExpenseService expenseService;
-	
-	@PostMapping("/create-expense")
+
+    private final ExpenseService expenseService;
+
+    @PostMapping("/create-expense")
     public ResponseEntity<ResponseStructure<ExpenseResponseDto>> createExpense(@RequestBody AddExpenseDto expense) {
         return expenseService.createExpense(expense);
     }
-	
-	@PutMapping("/update/{id}")
+
+    @PutMapping("/update/{id}")
     public ResponseEntity<ResponseStructure<ExpenseResponseDto>> updateExpense(
             @PathVariable Long id,
             @RequestBody AddExpenseDto updatedExpense
     ) {
         return expenseService.updateExpense(id, updatedExpense);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<ResponseStructure<ExpenseResponseDto>> deleteExpense(
+            @PathVariable Long id
+    ) {
+        return expenseService.deleteExpense(id);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ResponseStructure<ExpenseResponseDto>> getExpenseById(
+            @PathVariable Long id
+    ) {
+        return expenseService.getExpenseById(id);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<ResponseStructure<List<ExpenseResponseDto>>> getAllExpenses() {
+        return expenseService.getAllExpenses();
+    }
+
+    @GetMapping("/total")
+    public ResponseEntity<ResponseStructure<Double>> getTotalSpent() {
+        return expenseService.getTotalSpent();
     }
 }
